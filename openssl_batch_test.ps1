87161e59c10a631221f6a0f20679dbd7f20c1605
Blackstartech <blackstartech254@gmail.com>
2024-06-05 10:39:41 +0300

    removing openssl (so 3 days wasted)

--- openssl_batch_test.ps1
+++
@@ -1,6 +0,0 @@
-# winget install -e --id ShiningLight.OpenSSL
-# set PATH=%PATH%;"C:\Program Files\OpenSSL-Win64\bin"
-
-openssl genrsa -out tmp/yourdomain.key 2048 
-
-openssl req -new -key tmp/yourdomain.key -out tmp/yourdomain.csr -subj "/C=US/ST=Utah/L=Lehi/O=Your Company, Inc./OU=IT/CN=yourdomain.com"
--- openssl_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash          
-
-STR="Hello World!"
-echo $STR  
-
-for i in $(ls / -a -h -R| grep openssl); do 
-    echo file: $i
-done
